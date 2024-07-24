3d6d42bb34be459b8003cda01c63aedb85e06806
c_jean.silva <c_jean.silva@hapvida.com.br>
2024-07-24 19:15:09 -0300

    fix: fixed customer mapper

--- src/main/java/com/fiap/tc/adapter/repository/mapper/CustomerMapper.java
+++ src/main/java/com/fiap/tc/adapter/repository/mapper/CustomerMapper.java
@@ -4,14 +4,17 @@ import com.fiap.tc.adapter.repository.entity.CustomerEntity;
 import com.fiap.tc.adapter.repository.mapper.base.MapperEntity;
 import com.fiap.tc.core.domain.model.Customer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface CustomerMapper extends MapperEntity<CustomerEntity, Customer> {
 
     @Override
+    @Mapping(target = "active", source = "audit.active")
     Customer fromEntity(CustomerEntity customerEntity);
 
     @Override
+    @Mapping(target = "active", source = "audit.active")
     CustomerEntity toEntity(Customer customer);
 
 }
