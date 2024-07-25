0ad4b8d973950aa08023492d948abd254ef9bf31
Myller Sakaguchi Lobo <myller.lobo@omni.com.br>
2024-07-25 20:04:02 -0300

    fix error auth

--- src/main/java/com/fiap/tc/adapter/repository/UserRepository.java
+++ src/main/java/com/fiap/tc/adapter/repository/UserRepository.java
@@ -21,7 +21,7 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     public Optional<UserEntity> findByDocumentNumber(String documentNumber);
 
-    @Query(nativeQuery = true, value = "${sql.query.getRoles}")
+    @Query(nativeQuery = true, value = "SELECT p.id FROM seguranca.usuario u JOIN seguranca.usuario_perfil up ON u.id = up.id_usuario JOIN seguranca.perfil pa ON up.id_perfil = pa.id JOIN seguranca.perfil_permissao pp ON up.id_perfil = pp.id_perfil JOIN seguranca.permissao p ON pp.id_permissao = p.id WHERE u.id = :id ORDER BY p.id")
     Collection<String> getRoles(Long id);
 
 }
