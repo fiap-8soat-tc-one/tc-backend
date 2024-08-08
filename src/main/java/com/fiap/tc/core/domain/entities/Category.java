950b0505fa93aad2a9b60770715cd830f49fc59d
c_jean.silva <c_jean.silva@hapvida.com.br>
2024-08-08 15:25:02 -0300

    rmv: rmv swagger from domain category

--- src/main/java/com/fiap/tc/core/domain/entities/Category.java
+++ src/main/java/com/fiap/tc/core/domain/entities/Category.java
@@ -8,33 +8,8 @@ import java.util.UUID;
 @Data
 public class Category {
 
-    @ApiModelProperty(
-            value = "UUID Category",
-            example = "7ba2a960-2354-466f-8868-6ad713742407",
-            dataType = "UUID"
-    )
     private UUID id;
-
-    @ApiModelProperty(
-            value = "Category name",
-            required = true,
-            example = "lanche",
-            dataType = "String"
-    )
     private String name;
-
-    @ApiModelProperty(
-            value = "Category description",
-            required = true,
-            example = "categoria de lanche",
-            dataType = "String"
-    )
     private String description;
-
-    @ApiModelProperty(
-            value = "active status",
-            example = "true",
-            dataType = "Boolean"
-    )
     private Boolean active = true;
 }
